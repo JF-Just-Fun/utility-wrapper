8ccd49e03065a1917528d95547f0e54c2c1970c0
yinpo <347972265@qq.com>
2023-08-01 16:00:18 +0800

    fix: use @yinpo/node-scripts

--- scripts/exportAll.js
+++ scripts/exportAll.js
@@ -1,29 +1,3 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const url = require('node:url')
-const { Project } = require("ts-morph");
+#!/usr/bin/env node
 
-const srcDir = path.resolve(__dirname, '../src')
-
-const files = fs.readdirSync(srcDir)
-const exportContent = files.reduce((content, file) => {
-  const fileDir = path.resolve(__dirname, '../src', file)
-  if (fs.statSync(fileDir).isDirectory()) {
-    const project = new Project();
-    const sourceFile = project.addSourceFileAtPath(path.resolve(fileDir, 'index.ts'));
-
-    const hasDefaultExport = sourceFile.getExportSymbols().some(symbol =>
-        symbol.getName() === "default" || symbol.getDeclarations().some(declaration => declaration.isDefaultExport())
-    );
-
-    content += `export * from './src/${file}';\n`
-
-    if (hasDefaultExport) {
-      content += `export { default as ${file} } from './src/${file}';\n`
-    }
-  }
-
-  return content
-}, '')
-
-fs.writeFileSync(path.resolve(__dirname, '../index.ts'), exportContent, 'utf-8');
+require('@yinpo/node-scripts').exportAll([__dirname, '..'], 'src')
